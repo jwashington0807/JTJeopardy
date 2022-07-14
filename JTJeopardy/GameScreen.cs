7b62f7cadbd8020038cc860cb8405b1f7ee04a3c
JT Washington <jaytee.washington@gmail.com>
2022-07-14 13:32:10 -0400

    Add project files.

---
+++ JTJeopardy/GameScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JTJeopardy
+{
+    public partial class GameScreen : Form
+    {
+        HostScreen hostScreen = null;
+
+        public GameScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void GameScreen_Load(object sender, EventArgs e)
+        {
+            hostScreen = new HostScreen();
+            hostScreen.Show();
+        }
+    }
+}
---
+++ JTJeopardy/Utilities/Contestant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTJeopardy.Utilities
+{
+    internal class Contestant
+    {
+        string name = String.Empty;
+
+        Contestant(string _name)
+        {
+            this.name = _name;
+        }
+
+        int score = 0;
+        int getScore
+        {
+            get
+            {
+                return score;
+            }
+
+            set
+            {
+                this.score = value;
+            }
+        }
+    }
+}
